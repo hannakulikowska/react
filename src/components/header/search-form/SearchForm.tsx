7b8d6ace7450db4e19c46e4b3b5c8be2b569415d
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-07-07 15:43:07 +0200

    feat: add LocalStorage support for SearchForm component

--- src/components/header/search-form/SearchForm.tsx
+++ src/components/header/search-form/SearchForm.tsx
@@ -1,15 +1,44 @@
-import { Component } from 'react';
+import { Component, ChangeEvent, FormEvent } from 'react';
 import './SearchForm.scss';
+import {
+  saveSearchTermToLocalStorage,
+  getSearchTermFromLocalStorage,
+} from '../../../utils/localStorage';
 
-class SearchForm extends Component {
+interface SearchFormState {
+  searchTerm: string;
+}
+
+class SearchForm extends Component<object, SearchFormState> {
+  constructor(props: object) {
+    super(props);
+
+    const savedSearchTerm = getSearchTermFromLocalStorage();
+    this.state = {
+      searchTerm: savedSearchTerm.trim(),
+    };
+  }
+
+  handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const { searchTerm } = this.state;
+    saveSearchTermToLocalStorage(searchTerm);
+  };
+
   render() {
     return (
-      <form className="header__search-form search-form">
+      <form className="header__search-form search-form" onSubmit={this.handleSubmit}>
         <input
           className="search-form__input"
           aria-label="Search"
           placeholder="Search..."
           type="search"
+          value={this.state.searchTerm}
+          onChange={this.handleChange}
         />
         <button className="search-form__button button" type="submit">
           Search

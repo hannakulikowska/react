0ac662831d643f7b4f7531b5cde3938cf285e8b4
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-07-12 22:12:30 +0200

    refactor: delete unnecessary files with LS functions

--- src/utils/localStorage.ts
+++
@@ -1,7 +0,0 @@
-export const getSearchTermFromLocalStorage = (): string => {
-  return localStorage.getItem('searchTerm') ?? '';
-};
-
-export const saveSearchTermToLocalStorage = (searchTerm: string): void => {
-  localStorage.setItem('searchTerm', searchTerm.trim());
-};

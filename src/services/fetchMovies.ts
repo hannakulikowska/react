5bd3ccf19cfd385997656df64b9992f23cbb6181
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-07-12 19:47:25 +0200

    refactor: update fetchMovies function and rename api.ts to fetchMovies.ts

---
+++ src/services/fetchMovies.ts
@@ -0,0 +1,29 @@
+const BASE_URL = 'https://stapi.co/api/v1/rest/movie/search';
+
+interface ApiResponse {
+  movies: Movie[];
+}
+
+export interface Movie {
+  title: string;
+  usReleaseDate: string;
+}
+
+async function fetchMovies(): Promise<Movie[]> {
+  const url = new URL(`${BASE_URL}`);
+
+  const response: Response = await fetch(url.toString());
+
+  if (!response.ok) {
+    throw new Error('Fetch response was not successful');
+  }
+
+  const data: ApiResponse = (await response.json()) as ApiResponse;
+
+  return data.movies.map((movie) => ({
+    title: movie.title,
+    usReleaseDate: movie.usReleaseDate,
+  }));
+}
+
+export default fetchMovies;
